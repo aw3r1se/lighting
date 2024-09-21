ee09a982cd4710dfadcbd802d82c0c3afd2b0e39
aw3r1se <aw3r1se@gmail.com>
2024-09-21 19:14:05 +0500

    media

--- config/filesystems.php
+++ config/filesystems.php
@@ -30,17 +30,17 @@ return [
 
     'disks' => [
 
-        'local' => [
-            'driver' => 'local',
-            'root' => storage_path('app'),
-            'throw' => false,
-        ],
-
-        'public' => [
+        'media' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
+            'throw' => false,
+        ],
+
+        'local' => [
+            'driver' => 'local',
+            'root' => storage_path('app'),
             'throw' => false,
         ],
 
