3ef8e1db532165f0ce6c31f89f32a33ebf373545
Bogdan <kob@crtweb.ru>
2024-10-15 19:47:19 +0500

    api-docs

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use OpenApi\Attributes as OA;
+
+#[OA\Info(version: '1.0', title: 'Application')]
+#[OA\PathItem(path: 'api/docs')]
+#[OA\SecurityScheme(
+    securityScheme: 'auth',
+    type: 'http',
+    scheme: 'Bearer',
+)]
+#[OA\OpenApi(
+    security: [['auth' => []]]
+)]
 abstract class Controller
 {
     //
